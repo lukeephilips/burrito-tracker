fa26723b17d8288ca0550af339492a58cfb5ca43
Luke <luke.e.philips@gmail.com>
2017-01-20 12:07:08 -0800

    add boolean checkbox to new component

--- app/new-burrito.component.ts
+++ app/new-burrito.component.ts
@@ -14,9 +14,15 @@ export class NewBurritoComponent {
   test: string = "test";
 
   @Output() newBurritoSender = new EventEmitter();
-
   newBurrito(type: string, restaurant: string, isSuper: boolean, calories: number, date: Date) {
-    alert("shit works")
+    console.log(isSuper);
+    // if (isSuper == "true") {
+    //   isSuper = true;
+    // } else {
+    //   isSuper = false;
+    // }
+    // console.log(isSuper);
+
     let newBurrito: Burrito = new Burrito(type, restaurant, isSuper, calories, date)
     this.newBurritoSender.emit(newBurrito);
   }
